d06711f52e79283393bd087268e4c112326c8ab9
Christopher Wurm <christopher_wurm@outlook.de>
2025-07-31 20:12:27 +0200

    - DialogService in MAUI-Projekt erstellt - überflüssige nuget-Pakete entfernt

---
+++ Zugsichtungen.MAUI/Services/DialogService.cs
@@ -0,0 +1,13 @@
+﻿using Zugsichtungen.Abstractions.Interfaces;
+using Zugsichtungen.Abstractions.Services;
+
+namespace Zugsichtungen.MAUI.Services
+{
+    internal class DialogService : IDialogService
+    {
+        public Task<object?> ShowDialog(ILoadable viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
