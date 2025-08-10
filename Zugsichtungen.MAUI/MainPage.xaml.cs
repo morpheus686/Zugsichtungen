2a1b3be2e15e005f06b4bf06074238556eb00173
Christopher Wurm <christopher_wurm@outlook.de>
2025-08-10 12:45:25 +0200

    - MainPage ist nun erstmal bedienbar

--- Zugsichtungen.MAUI/MainPage.xaml.cs
+++ Zugsichtungen.MAUI/MainPage.xaml.cs
@@ -21,17 +21,5 @@ namespace Zugsichtungen.MAUI
                 await mwvm.Initialize();
             }
         }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 }
